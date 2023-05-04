8eaa3ebc949c3da62c9896ddb6a9561908a93e1d
Madam <adammiernicki98@gmail.com>
2023-05-04 07:08:21 +0200

    photo fixed

---
+++ client/src/assets/index.js
@@ -0,0 +1,51 @@
+import adam from "./adam.png";
+import blockchain from "./blockchain.svg";
+import cellphone from "./cellphone.svg";
+import chainfit from "./chainfit.png";
+import close from "./close.svg";
+import coin from "./coin.svg";
+import dumbbells from "./dumbbells.svg";
+import facebookIcon from "./facebook.png";
+import facebook from "./facebook.svg";
+import googlesoon from "./googlesoon.png";
+import gymwalking from "./gymwalking.svg";
+import instafb from "./instafb.svg";
+import instagramIcon from "./instagram.png";
+import instagram from "./instagram.svg";
+import julia from "./julia.png";
+import linkedinIcon from "./linkedin.png";
+import linkedin from "./linkedin.svg";
+import menu from "./menu.svg";
+import metamask from "./metamask.png";
+import milosz from "./milosz.jpg";
+import mobileph from "./mobileph.svg";
+import money from "./money.svg";
+import phantom from "./phantom.png";
+import twitter from "./twitter.svg";
+
+export {
+  adam,
+  blockchain,
+  cellphone,
+  chainfit,
+  close,
+  coin,
+  dumbbells,
+  facebook,
+  facebookIcon,
+  googlesoon,
+  gymwalking,
+  instafb,
+  instagram,
+  instagramIcon,
+  julia,
+  linkedin,
+  linkedinIcon,
+  menu,
+  metamask,
+  milosz,
+  mobileph,
+  money,
+  phantom,
+  twitter,
+};
