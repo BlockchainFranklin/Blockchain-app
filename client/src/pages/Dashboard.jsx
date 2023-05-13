6cc1e0c924fc6aa52850fd5337661edf5ff9fea3
Miłosz <45383427+Althal@users.noreply.github.com>
2023-05-13 04:15:32 +0200

    Add files via upload

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import { Statistics } from '../components';
 import { cardsData, groupNumber } from '../constants';
 
+import { ethBalance, cftBalance } from '../web3/SmartContract.jsx';
+
+
+let ethBalanceText = await ethBalance();
+ethBalanceText = parseFloat(ethBalanceText/10**18).toFixed(2);
+
+let cftBalanceText = await cftBalance();
+cftBalanceText = parseFloat(cftBalanceText/10**18).toFixed(2);
+
 const Dashboard = () => {
   return <div className="containerDashboard">
     {/* left side */}
@@ -21,12 +30,40 @@ const Dashboard = () => {
         {cardsData.map((card, index)=> (
           <div className="cardDashboard" key={index}>
             <div className="cardHeadDashboard">
-              <span>{card.title}</span>
-              <span>+{card.change}</span>
+              <span>ETH</span>
+              <span></span>
             </div>
             <div className="cardAmountDashboard">
               <span>$</span>
-              <span>{groupNumber(card.amount)}</span>
+              <span>{ethBalanceText}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      </div>
+
+
+      <div className="dashboardHeadDashboard theme-container">
+        <div className="headDashboard">
+          <h6>Dashboard</h6>
+          <div className="durationButtonDashboard">
+            <select>
+              <option value="">1 week</option>
+              <option value="">1 month</option>
+              <option value="">1 year</option>
+            </select>
+          </div>
+        </div>
+        <div className="cardsDashboard">
+        {cardsData.map((card, index)=> (
+          <div className="cardDashboard" key={index}>
+            <div className="cardHeadDashboard">
+              <span>CFT</span>
+              <span></span>
+            </div>
+            <div className="cardAmountDashboard">
+              <span>$</span>
+              <span>{cftBalanceText}</span>
             </div>
           </div>
         ))}
